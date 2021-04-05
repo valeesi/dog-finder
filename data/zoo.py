c07526d664f1b1df6b3a75bf57a2bce572a19e86
Allan Wang <16047959+Valeesi@users.noreply.github.com>
2021-04-05 15:53:01 +0200

    Refactoring

--- data/zoo.py
+++ data/zoo.py
@@ -1,3 +1,6 @@
+from pyquery import PyQuery as pq
+import hashlib
+
 sites_to_monitor = {
     'hundarutanhem': {
         'url': 'https://hundarutanhem.se/hundar/',
@@ -14,3 +17,27 @@ sites_to_monitor = {
 # 'hundrondellen': 'https://hundhjalpen.se/hundar-for-adoption/',
 # 'sosanimals': 'https://www.sos-animals.se/dogs/'
 dogs_found = {''}
+
+
+def add_dog(url, tag_for_list, additional_tag=None):
+    d = pq(url)
+    for dog in d.items(tag_for_list):
+        if additional_tag is None:
+            dog_entry = dog
+        else:
+            dog_entry = dog.find(additional_tag)
+        dogs_found.add(
+            hash_dog(dog_entry.__str__().encode("utf-8"))
+        )
+
+
+def load_current_dogs():
+    for site, value in sites_to_monitor.items():
+        if site == 'hundarutanhem':
+            add_dog(value['url'], 'article', additional_tag='a')
+        elif site == 'hundarsokerhem':
+            add_dog(value['url'], value['tag'])
+
+
+def hash_dog(str):
+    return hashlib.sha224(str).hexdigest()
--- main.py
+++ main.py
@@ -1,36 +1,9 @@
 import time
-import hashlib
-from pyquery import PyQuery as pq
-import mail.factory as mail
-import data.zoo as data
+import data.zoo as zoo
 
 time_out = 10
-
-def hash(str):
-    return hashlib.sha224(str).hexdigest()
-
-
-def add_dog(url, tag_for_list, additional_tag=None):
-    d = pq(url)
-    for dog in d.items(tag_for_list):
-        if additional_tag is None:
-            dog_entry = dog
-        else:
-            dog_entry = dog.find(additional_tag)
-        data.dogs_found.add(
-            hash(dog_entry.__str__().encode("utf-8"))
-        )
-
-def load_current_dogs():
-    for site, value in data.sites_to_monitor.items():
-        if site == 'hundarutanhem':
-            add_dog(value['url'], 'article', additional_tag='a')
-        elif site == 'hundarsokerhem':
-            add_dog(value['url'], value['tag'])
-
-
 print("Dog finder launched")
-strr = load_current_dogs()
+strr = zoo.load_current_dogs()
 # mail.SendMail("New dogs found", strr)
 print("Mail sent")
 
