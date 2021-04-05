78b12f2edf1ff14762b9d624beb1eb6560e780f4
Allan Wang <16047959+Valeesi@users.noreply.github.com>
2021-04-05 12:03:41 +0200

    Can now send email notifications with embedded images

--- dog_finder.py
+++ dog_finder.py
@@ -2,31 +2,27 @@ import time
 import hashlib
 import smtplib
 import secrets.mail as mail
-from urllib.request import urlopen, Request
 from email.mime.text import MIMEText
-from html.parser import HTMLParser
 from pyquery import PyQuery as pq
-from lxml import etree
-import urllib
 
+from email.mime.multipart import MIMEMultipart
 
-dog_website = "https://hundarutanhem.se/dog/category/sma-hundar/"
 time_out = 10
-url = Request(dog_website, headers={'User-Agent': 'Mozilla/5.0'})
-
-response = urlopen(url).read().decode("utf-8")
-# d = pq(response)
 d = pq('https://hundarutanhem.se/dog/category/sma-hundar/')
 first_dog_href = d("article").eq(0).find('a').attr("href")
 first_dog_img = d("article").eq(0).find('a').find("img").attr("src")
 
-currentHash = hashlib.sha224(response).hexdigest()
+currentHash = hashlib.sha224(d("article").__str__().encode("utf-8")).hexdigest()
 print("Dog finder launched")
 
-msg = MIMEText("Random text", 'plain')
+msg = MIMEMultipart("First dog")
 msg['Subject'] = "Test sent from Python"
 msg['From'] = mail.username
 msg['To'] = ", ".join(mail.recipients)
+msgText = MIMEText(
+    '<a href="https://hundarutanhem.se/hundar/pigga-21-0256/"><img src="https://hundarutanhem.se/wp-content/uploads/2021/03/Pigga-21-0256_003.jpg" alt=""></a>',
+    'html')
+msg.attach(msgText)
 
 mail_server = smtplib.SMTP('smtp.office365.com', 587)
 mail_server.ehlo()
@@ -37,9 +33,9 @@ mail_server.login(mail.username, mail.password)
 while True:
     try:
         time.sleep(time_out)
-        response = urlopen(url).read()
-        newHash = hashlib.sha224(response).hexdigest()
+        d = pq('https://hundarutanhem.se/dog/category/sma-hundar/')
 
+        newHash = hashlib.sha224(d("article").__str__().encode("utf-8")).hexdigest()
         if newHash == currentHash:
             continue
         else:
