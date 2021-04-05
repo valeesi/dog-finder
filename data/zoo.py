c15034b6a15edc4fc5b5d40babb1a20acfae2cbd
Allan Wang <16047959+Valeesi@users.noreply.github.com>
2021-04-05 15:48:12 +0200

    Add support for multiple sites

---
+++ data/zoo.py
@@ -0,0 +1,16 @@
+sites_to_monitor = {
+    'hundarutanhem': {
+        'url': 'https://hundarutanhem.se/hundar/',
+        'tag': 'a'
+    },
+    'hundarsokerhem': {
+        'url': 'https://hundarsokerhem.se/hundar-som-soker-hem/',
+        'tag': '.arkivhund'
+    }
+}
+# 'hundstallet': 'https://hundstallet.se/soker-hem/',
+# 'dogrescue': 'https://www.dogrescue.se/hundar-soker-hem/',
+# 'hundhjalpen': 'https://hundhjalpen.se/hundar-for-adoption/',
+# 'hundrondellen': 'https://hundhjalpen.se/hundar-for-adoption/',
+# 'sosanimals': 'https://www.sos-animals.se/dogs/'
+dogs_found = {''}
--- main.py
+++ main.py
@@ -2,18 +2,38 @@ import time
 import hashlib
 from pyquery import PyQuery as pq
 import mail.factory as mail
+import data.zoo as data
 
 time_out = 10
-d = pq('https://hundarutanhem.se/dog/category/sma-hundar/')
-first_dog_href = d("article").eq(0).find('a').attr("href")
-first_dog_img = d("article").eq(0).find('a').find("img").attr("src")
 
-currentHash = hashlib.sha224(d("article").__str__().encode("utf-8")).hexdigest()
+def hash(str):
+    return hashlib.sha224(str).hexdigest()
+
+
+def add_dog(url, tag_for_list, additional_tag=None):
+    d = pq(url)
+    for dog in d.items(tag_for_list):
+        if additional_tag is None:
+            dog_entry = dog
+        else:
+            dog_entry = dog.find(additional_tag)
+        data.dogs_found.add(
+            hash(dog_entry.__str__().encode("utf-8"))
+        )
+
+def load_current_dogs():
+    for site, value in data.sites_to_monitor.items():
+        if site == 'hundarutanhem':
+            add_dog(value['url'], 'article', additional_tag='a')
+        elif site == 'hundarsokerhem':
+            add_dog(value['url'], value['tag'])
+
+
 print("Dog finder launched")
+strr = load_current_dogs()
+# mail.SendMail("New dogs found", strr)
+print("Mail sent")
 
-mail.SendMail("New dogs found", d("article").eq(0).__str__())
-
-#
 # while True:
 #     try:
 #         time.sleep(time_out)
