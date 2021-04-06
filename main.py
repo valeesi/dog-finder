6a6632513031866fa9053b1c2aaf41c4b8f07433
Allan Wang <16047959+Valeesi@users.noreply.github.com>
2021-04-06 19:54:00 +0200

    * Fix mailing bug

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from mail.factory import SendMail
 
 def main():
     logger.info("Launching...")
+    logger.info("Search for dogs on " + len(zoo.sites_to_monitor.items()).__str__() + " sites")
     zoo.scan_for_dogs()
+    logger.info(len(zoo.current_dogs).__str__() + " dogs have been added to cache")
     zoo.new_dogs.clear()
     logger.info("Monitoring...")
     while True:
         try:
-            time.sleep(300)
+            time.sleep(600)
             zoo.scan_for_dogs()
             if len(zoo.new_dogs) == 0:
+                logger.info("No updates...")
                 continue
             else:
-                notis = len(zoo.new_dogs.values()).__str__() + " new dogs found"
+                notis = len(zoo.new_dogs.values()).__str__() + " new dog(s) found"
                 SendMail(notis, "".join(zoo.new_dogs.values()))
                 logger.info(notis)
                 zoo.new_dogs.clear()
